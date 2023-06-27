70ac9ad55f69b6b9f3888ca1ec0bc45adbb0a82f
JorgeEstacio <jorge.estacio@correounivalle.edu.co>
2023-06-26 22:46:22 -0500

    actualización y correciones del punto 1 funcional

--- punto1.py
+++ punto1.py
@@ -5,9 +5,11 @@ Punto 1
 Desarrolle la operación combinar(Lista L1, Lista L2) que recibe dos listas simplemente enlazadas,
 cada una ordenada ascendentemente, y devuelve una lista L3 que tiene los elementos ordenados de ambas
 listas. Las listas pueden tener elementos repetidos.
+
+se implica que las listas están ordenadas ascendentemente para este ejercicio
 """
+
 from listaEnlazada import ListaEnlazada as len
-#from nodo import Nodo
 
 def combinar (L1, L2):
     # Verificar si alguna de las listas está vacía (caso triviial)
@@ -17,61 +19,87 @@ def combinar (L1, L2):
         return L1
     
     # Crear una lista enlazada vacía para almacenar la combinación
-    L3 = None
-    actual = None
+    nuevaLista = len()
+    #actual = None
 
     # Recorrer ambas listas simultáneamente
-    while len.verificarVacio(L1) and len.verificarVacio(L2):
-        if len.traerIndice(L1) <= len.traerIndice(L2):
+    while len.verificarVacio(L1) is False and len.verificarVacio(L2) is False:
+        if len.traerIndice(L1, 0) <= len.traerIndice(L2, 0):
             #Se agrega el nodo de L1 a L3 y se borra de L1
-            len.insertar(L3, len.traerIndice(L1))
-            len.borrar(L1, len.traerIndice(L1))
+            len.insertar(nuevaLista, len.traerIndice(L1, 0))
+            len.borrar(L1, len.traerIndice(L1, 0))
         else:
             #Se agrega el nodo de L2 a L3 y se borra de L2
-            len.insertar(L3, len.traerIndice(L2))
-            len.borrar(L2, len.traerIndice(L2))
+            len.insertar(nuevaLista, len.traerIndice(L2, 0))
+            len.borrar(L2, len.traerIndice(L2, 0))
 
     #Agregar los nodos restantes de L1 si L2 ya se vació
-    while len.verificarVacio(L1):
-        len.insertar(L3, len.traerIndice(L1))
-        len.borrar(L1, len.traerIndice(L1))
+    while len.verificarVacio(L1) is False:
+        len.insertar(nuevaLista, len.traerIndice(L1, 0))
+        len.borrar(L1, len.traerIndice(L1, 0))
     
     #Agregar los nodos restantes de L2 si L1 ya se vació
-    while len.verificarVacio(L2):
-        len.insertar(L3, len.traerIndice(L2))
-        len.borrar(L2, len.traerIndice(L2))
+    while len.verificarVacio(L2) is False:
+        len.insertar(nuevaLista, len.traerIndice(L2, 0))
+        len.borrar(L2, len.traerIndice(L2, 0))
     
-    return L3
+    return nuevaLista
 
 # Definir las listas
-# Lista 1: 1 -> 3 -> 5 -> 7
 
-L1 = len(None)
+#Ejemplo 1:
+
+# Lista 1: 1 -> 3 -> 5 -> 7
+L1 = len()
 len.insertar(L1, 1)
 len.insertar(L1, 3)
 len.insertar(L1, 5)
 len.insertar(L1, 7)
 len.insertar(L1, 9)
-
-
 # Lista 2: 2 -> 4 -> 6 -> 8
-L2 = len(None)
+L2 = len()
 len.insertar(L2, 2)
 len.insertar(L2, 4)
 len.insertar(L2, 6)
 len.insertar(L2, 8)
-
-
-# Llamar a la función combinar
+# Llamar a la función combinar y mostrar el resultado
 L3 = combinar(L1, L2)
+print("Ejemplo 1:")
+len.recorrido(L3)
 
-#mostrar el primer indice de L3
-print(len.traerIndice(L3,1))
+#Ejemplo 2:
 
-# Imprimir la lista combinada
-"""
-while not(len.verificarVacio(L3)):
-    print(len.traerIndice(L3), end=" -> ")
-    len.borrar(L3,len.traerIndice(L3))
-print("None")
-"""
+# Lista 1: 1 -> 2 -> 2 -> 7 -> 15
+L4 = len()
+len.insertar(L4, 1)
+len.insertar(L4, 2)
+len.insertar(L4, 2)
+len.insertar(L4, 7)
+len.insertar(L4, 15)
+# Lista 2: 0 -> 4 -> 7 -> 10 -> 20
+L5 = len()
+len.insertar(L5, 0)
+len.insertar(L5, 4)
+len.insertar(L5, 7)
+len.insertar(L5, 10)
+len.insertar(L5, 20)
+# Llamar a la función combinar y mostrar el resultado
+L6 = combinar(L4, L5)
+print("Ejemplo 2:")
+len.recorrido(L6)
+
+#Ejemplo 3:
+
+# Lista 1: None
+L7 = len()
+# Lista 2: 5 -> 10 -> 15 -> 20 -> 25
+L8 = len()
+len.insertar(L8, 5)
+len.insertar(L8, 10)
+len.insertar(L8, 15)
+len.insertar(L8, 20)
+len.insertar(L8, 25)
+# Llamar a la función combinar y mostrar el resultado
+L9 = combinar(L7, L8)
+print("Ejemplo 3:")
+len.recorrido(L9)
